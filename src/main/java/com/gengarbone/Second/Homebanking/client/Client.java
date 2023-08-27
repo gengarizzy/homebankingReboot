2aac6309aa46486957887bb08b01daa3e619dea1
gengarbone <75919423+gengarizzy@users.noreply.github.com>
2023-08-26 23:26:57 -0300

    PUSH sprint1

---
+++ src/main/java/com/gengarbone/Second/Homebanking/client/Client.java
@@ -0,0 +1,69 @@
+package com.gengarbone.Second.Homebanking.client;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Client {
+
+//PROPERTIES
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+
+
+//CONSTRUCTORS
+    public Client() {
+    }
+
+    public Client(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+
+
+
+
+
+
+//GETTERS AND SETTERS
+    public Long getId() {
+        return id;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+
+
+
+}
---
+++ src/main/java/com/gengarbone/Second/Homebanking/client/ClientRepository.java
@@ -0,0 +1,8 @@
+package com.gengarbone.Second.Homebanking.client;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
+
+
+}
