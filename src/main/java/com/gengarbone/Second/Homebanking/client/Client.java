bad93cc87745e9806a30868c2128360acc99f8bc
gengarbone <75919423+gengarizzy@users.noreply.github.com>
2023-08-26 23:29:06 -0300

    PUSH sprint1

--- src/main/java/com/gengarbone/Second/Homebanking/client/Client.java
+++ src/main/java/com/gengarbone/Second/Homebanking/client/Client.java
@@ -1,9 +1,11 @@
 package com.gengarbone.Second.Homebanking.client;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class Client {
 
 //PROPERTIES
