44239b3f69a99d2ac85297d64994d816ff95f5e3
gengarbone <75919423+gengarizzy@users.noreply.github.com>
2023-08-26 23:14:58 -0300

    Project Start

---
+++ src/main/java/com/gengarbone/Second/Homebanking/SecondHomebankingApplication.java
@@ -0,0 +1,13 @@
+package com.gengarbone.Second.Homebanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondHomebankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondHomebankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gengarbone/Second/Homebanking/SecondHomebankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gengarbone.Second.Homebanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondHomebankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
